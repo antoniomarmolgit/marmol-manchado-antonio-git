c691ca1a6d8ac27b33b31fafe9c2c2fd4a291a72
Antonio Marmol <foobar>
2012-05-15 18:10:06 +0200

    Primer commit

---
+++ pila.java
@@ -0,0 +1,49 @@
+/*
+*@author Antonio Marmol
+ * 
+ */
+
+import java.util.ArrayList;
+
+public class Pila<T> {
+	private ArrayList<T> pila = null;
+
+	Pila() {
+
+		pila = new ArrayList<T>();
+
+	}
+
+	void push(T element) {
+		pila.add(element);
+	}
+
+	T pop() {
+
+		if (pila.size() == 0)
+			return null;
+		return pila.remove(pila.size() - 1);
+
+	}
+
+	T top() {
+
+		if (pila.size() == 0)
+			return null;
+		return pila.get(pila.size() - 1);
+
+	}
+
+	void show() {
+		System.out.println(pila);
+
+	}
+	
+	boolean IsEmpty (){
+		if (pila.size()==0)
+		return true;
+		return false;
+	}
+
+}
+
